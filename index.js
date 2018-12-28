6239d540a697526611738eca943444c32908f31f
Ennary <31353526+Ruuv1337@users.noreply.github.com>
2018-12-28 11:28:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,25 +8,145 @@
 //proxy rl - Teleport to right ladder for Corsair Stringhold.
 //proxy hide - Teleport to "hide zone" for Corsair Stringhold.
 
-//proxy kuma ## - Correct Z Position NEW!
+//proxy shift Z TIME - NEW Conf sorc Incredible (Only Warp Barrier)
+//proxy kuma ## - Correct Z Position NEW! (Disabled)
 
 module.exports = function RTPort(mod) {
+
+let enabled = false;
+let shift = -500; //you Z coord conf
+let secdef = 4300 //ms for incredible
+
 	let xyz = [];
   let svx = 0;
   let svy = 0;
   let svz = 0;
   
-	mod.hook('S_LOGIN', 10, (event) => {id = event.gameId})
+  let bkp = false
+  let block = false
+  
+  let id = 0; //you client id
+  let model = 0; //you class
+  
+  let skillid = 0; //current skill id
+  let mask = []; //mask work skill id
+  let mask_size = 0;
+
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+function IncMask (inc, size, msa) {
+	  for(var i = 0; i < inc.length; i++) {
+        for(var n = 0; n < size; n++) {
+            if(inc.indexOf(msa[n]) != -1) {
+                return n;
+            }}
+  }
+  return -1
+}
+
+function findInArray(ary, item) {
+    for (let i = 0; i < ary.length; i++) {
+    if (ary[i].toString() === item.toString()) {
+   return i
+   }}
+    return -1
+}
+/////////////////////////////////////////
+/////////////////////////////////////////
+/////////////////////////////////////////
+
+function Reload() {
+	enabled = false
+	setTimeout(500)
+	block = false
+  mod.command.message('<font color="#00ffff">[RTPort]</font> You alredy on ground!')
+}
+
+mod.hook('C_START_SKILL', 7, event => {
+  skillid = event.skill.toString()
+  let filter = IncMask(skillid, mask_size, mask)
+  
+if (!block){
+if (filter == -1) {
+		//console.log('S_FILTER: ' + event.skill)
+    return
+}else{
+    //console.log('S_PASS: ' + event.skill)
+    bkp = xyz[2]
+    enabled = true
+    mod.command.message('<font color="#00ffff">[RTPort]</font> You are UNDERGROUND!')
+    block = true
+    setTimeout(Reload, secdef)
+    return false
+}
+}else{
+return false}
+})
+
+	mod.hook('S_LOGIN', 12, (event) => {
+	id = event.gameId;
+	model = event.templateId % 100
+	
+//# warrior = 1, lancer = 2, slayer = 3, berserker = 4,
+//# sorcerer = 5, archer = 6, priest = 7, mystic = 8,
+//# reaper = 9, gunner = 10, brawler = 11, ninja = 12,
+//# valkyrie = 13
+
+		if (model == 5){
+		mask = ["A310110", "A310120"]
+		}else{
+		mask = []
+		}
+		mask_size = mask.length
+})
+	
 	mod.hook('C_PLAYER_LOCATION', 1, (event) => {
 		xyz[0] = event.x2
 		xyz[1] = event.y2
 		xyz[2] = event.z2
 		xyz[4] = event.time
 		xyz[5] = event.w
+	if (shift === 0) return
+  if (!enabled) {
+   event.z1 = bkp
+   event.z2 = bkp
+   return true
+  }else{
+   event.z1 += shift
+   event.z2 += shift
+   return true
+   }
 	})
+	
+  mod.hook('S_ACTION_STAGE', 9, (event) => {
+      if (!enabled) return
+      if (shift === 0) return
+      if (event.gameId.toString() !== id.toString()) return
+      event.loc = xyz[0], xyz[1], xyz[2]
+      return true
+  })
+  
+  mod.hook('S_ACTION_END', 5, (event) => {
+      if (!enabled) return
+      if (shift === 0) return
+      if (event.gameId.toString() !== id.toString()) return
+      event.loc = xyz[0], xyz[1], xyz[2]
+      return true
+  })
+	
+	
 	mod.hook('S_LOAD_TOPO', 1, (event) => {
 		xyz[3] = event.zone})
 
+  mod.command.add('shift', (offset1, offset2) => {
+        shift = parseFloat(offset1)
+        secdef = parseFloat(offset2)
+        if (secdef <= 1000) secdef = 1000
+        if (shift > 0) shift = -500
+        mod.command.message('Shift set to ' + shift + 'and time set to ' + secdef)
+    })
+    
   mod.command.add('coord', () => {
 		mod.command.message(`<font color="#00ffff"><font color="#ffff00">ZONE:</font>${xyz[3]} <font color="#ffff00">X:</font>${xyz[0]} <font color="#ffff00">Y:</font>${xyz[1]} <font color="#ffff00">Z:</font>${xyz[2]}</font>`)
 		svx = xyz[0]
@@ -52,10 +172,36 @@ module.exports = function RTPort(mod) {
 	}
 	})
 	
+	
+//Test Fish
+//		mod.hook('C_atata', 1, (event) => {
+//		event.ok = 1
+//		return true
+//		})
+		
+//	 mod.command.add('ok', () => {
+//		mod.toServer('C_atata', 1,{
+//                    ok: 1})
+//		})
+
+	
 	mod.command.add('tp', (argx, argy, argz) => {
-		arg1 = parseFloat(argx);
-		arg2 = parseFloat(argy);
-		arg3 = parseFloat(argz);
+   let arg1 = xyz[0]
+   let arg2 =	xyz[1]
+   let arg3 =	xyz[2]
+   
+	if (argx != 0) {
+    arg1 = parseFloat(argx);
+	}
+	
+	if (argy != 0) {
+    arg2 = parseFloat(argy);
+	}
+	
+  if (argz != 0) {
+    arg3 = parseFloat(argz);
+	}
+	
 		mod.toClient('S_INSTANT_MOVE', 1,{
                     id: id,
                     x: arg1,
@@ -70,22 +216,22 @@ module.exports = function RTPort(mod) {
 	// ## KR- KUMAS Royale ## //
 	// ###################### //
 	
-	let kuma = 0;
-	
-	mod.command.add('kuma', (offset) => {
-	if (116 === xyz[3]) {
-		kuma = parseFloat(offset)
-		mod.command.message('<font color="#ffff00">Position correct to '+(kuma)+'.</font>')
-		   }else{
-    mod.command.message('<font color="#00ffff">[RTPort]</font> <font color="#ffff00">Only Kuma Royale!</font>');}
-	})
-	
-	mod.hook('C_VEHICLEEX_LOCATION', 1, (event) => {
-		if (kuma === 0) return
-		event.z1 += shift
-		event.z2 += shift
-		return true
-	})
+//	let kuma = 0;
+//	
+//	mod.command.add('kuma', (offset) => {
+//	if (116 === xyz[3]) {
+//		kuma = parseFloat(offset)
+//		mod.command.message('<font color="#ffff00">Position correct to '+(kuma)+'.</font>')
+//		   }else{
+//    mod.command.message('<font color="#00ffff">[RTPort]</font> <font color="#ffff00">Only Kuma Royale!</font>');}
+//	})
+//	
+//	mod.hook('C_VEHICLEEX_LOCATION', 1, (event) => {
+//		if (kuma === 0) return
+//		event.z1 += shift
+//		event.z2 += shift
+//		return true
+//	})
 	
 	// ###################### //
 	// ### CS- STRONGHOLD ### //
